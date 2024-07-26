3fca12a1061bf48c6ab006d0701efd4b97586f49
Pobitro Deb <pobitrodn@gmail.com>
2024-07-26 20:52:21 +0600

    Added Plugin Information

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Plugin Name: Scroll To Top With Animation
+ * Plugin URI: https://wordpress.org/plugins/simple-to-top-with-animation 
+ * Description: This is simple scroll to top plugin will help you to enable back to top button to your wordpress website. 
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.3 
+ * Author: Pobitro Deb
+ * Author URL: http://pobitrodeb.com/ 
+ * License: GPLv2 or latter 
+ * License URL: https://www.gnu.org/licenses/gpl-1.0.0.html
+ * Update URI: https://github.com/pobitrodeb 
+ * Text Domain: sstt
+ */
+
+?> 
+ 
